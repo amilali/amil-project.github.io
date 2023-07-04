cd0bfdad16f2c85ff8379b62499625fa55271e6c
Amil Ali <amil13985@gmail.com>
2023-07-04 20:42:09 +0530

    add

--- Quote_generator/script.js
+++ Quote_generator/script.js
@@ -1,3 +1,10 @@
+const twitterbtn = document.getElementById("twitter");
+const quotebtn = document.getElementById("new-quote");
+const quoteContainer = document.getElementById("quote-container");
+const authortext = document.getElementById("author");
+const quotetext = document.getElementById("quote");
+
+
 // Get quotes from api
 let apiQuotes = [];
 
@@ -5,9 +12,17 @@ let apiQuotes = [];
 function newQuote(){
     // pick random quotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote); 
-    document.getElementById("quote").innerHTML = quote.text;
-    document.getElementById("author").innerHTML = quote.author ;
+    // console.log(quote); 
+
+    //check if author is nul
+    if(!quote.author)
+    {
+        authortext.textContent = 'Unknown'; 
+    }
+    else{
+    authortext.textContent = quote.author;
+    }
+    quotetext.textContent = quote.text;
 }
 
 
