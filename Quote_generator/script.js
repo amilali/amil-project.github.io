2150cc2a6b9309dfccf9791fe229f62ae38bfd09
Amil Ali <amil13985@gmail.com>
2023-07-05 20:06:31 +0530

    add

--- Quote_generator/script.js
+++ Quote_generator/script.js
@@ -3,13 +3,28 @@ const quotebtn = document.getElementById("new-quote");
 const quoteContainer = document.getElementById("quote-container");
 const authortext = document.getElementById("author");
 const quotetext = document.getElementById("quote");
+const loader = document.getElementById("loader")
 
+// loader
+function loading()
+{
+    quoteContainer.hidden = true;
+    loader.hidden=false;
 
+}
+
+function complete()
+{
+    quoteContainer.hidden = false;
+    loader.hidden= true;
+
+}
 // Get quotes from api (2)
 let apiQuotes = [];
 
 // new quote
 function newQuote(){
+    loading();
     // pick random quotes
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // console.log(quote); 
@@ -23,12 +38,14 @@ function newQuote(){
     authortext.textContent = quote.author;
     }
     quotetext.textContent = quote.text;
+    complete();
 }
 
 
 // make api call (1)
 async function getQuotes()
 {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
             const response = await fetch(apiUrl);
@@ -38,6 +55,7 @@ async function getQuotes()
 
             // catch error here
     }
+
 }
 
 // tweet (3)
@@ -49,6 +67,9 @@ function tweetQuote(){
 // Event lisner
 quotebtn.addEventListener('click', newQuote);
 twitterbtn.addEventListener('click', tweetQuote);
+
+
+
 
 // on load
 getQuotes();
