86174df874ef95b99e0dfea2894eb4fcdc3ab96f
Amil Ali <amil13985@gmail.com>
2023-07-07 13:18:42 +0530

    add

--- promises_practices/script.js
+++ promises_practices/script.js
@@ -1,14 +1,11 @@
 function getpromises() {
-    return fetch('https://type.fit/ap/quotes');
+    return fetch('https://type.fit/api/quotes');
   }
   const get = getpromises();
   console.log(get);
   
 let arr = new Array();
 
-get.catch(error=>{
-    console.log(error)
-})
 
   get.then(response => {
     return response.json();
@@ -17,5 +14,31 @@ get.catch(error=>{
     arr=data;
     printarr(arr);
   });
-let h1 = document.getElementById("amil");
-  printarr =(arr)=>h1.textContent=arr[Math.floor(Math.random()*arr.length)].text;
+  //error
+  get.catch(error => {
+    displayError(error);
+  });
+   //another way of writing
+//    async function fetchData() {
+//     try {
+//       const response = await fetch('https://type.fit/api/quotes');
+//       const data = await response.json();
+//       printarr(data);
+//     } catch (error) {
+//       displayError(error);
+//     }
+//   }
+  
+//   fetchData();
+
+
+  printarr =(arr)=>{
+    let h1 = document.getElementById("amil");
+    h1.textContent=arr[Math.floor(Math.random()*arr.length)].text;
+  }
+
+  // if error occured
+  function displayError(error) {
+    let errorElement = document.getElementById("amil");
+    errorElement.textContent = "An error occurred: " + error;
+  }
